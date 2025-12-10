7aa92ff176ff3f9bc275bf0ce55f7bc89ff1fb23
Hrishikesh Terdalkar <hrishikeshrt@users.noreply.github.com>
2025-12-10 17:41:25 +0530

    build(release): bump version 0.1.2 → 1.0.0

--- setup.py
+++ setup.py
@@ -49,6 +49,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/hrishikeshrt/heritage',
-    version='0.1.2',
+    version='1.0.0',
     zip_safe=False,
 )
