1a39f4db4beb4dabc033e4224ec6bff60e799225
Hrishikesh Terdalkar <hrishikeshrt@users.noreply.github.com>
2025-12-10 21:43:27 +0530

    chore: update Python support, Sphinx, and readthedocs config

--- setup.py
+++ setup.py
@@ -4,15 +4,16 @@
 
 from setuptools import setup, find_packages
 
-with open('README.rst') as readme_file:
+with open('README.rst', encoding='utf-8') as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
+with open('HISTORY.rst', encoding='utf-8') as history_file:
     history = history_file.read()
 
+# Runtime dependencies for end users of the library/CLI.
 requirements = [
     "requests>=2.26.0",
-    "beautifulsoup4>=4.10.0"
+    "beautifulsoup4>=4.10.0",
 ]
 
 test_requirements = ['pytest>=3', ]
@@ -20,18 +21,20 @@ test_requirements = ['pytest>=3', ]
 setup(
     author="Hrishikesh Terdalkar",
     author_email='hrishikeshrt@linuxmail.org',
-    python_requires='>=3.6',
+    python_requires='>=3.8',
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
     ],
     description="Python Interface to The Sanskrit Heritage Site",
     entry_points={
